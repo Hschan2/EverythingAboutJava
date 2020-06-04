c7c2f45d10800256159abcc7194e8ff6c5fbf08b
HONGSEONGCHAN <seongchan_@naver.com>
2020-06-04 20:39:35 +0900

    Inflearn

--- Inflearn/src/com/java/objectProgramming/objectProgramming.java
+++ Inflearn/src/com/java/objectProgramming/objectProgramming.java
@@ -5,8 +5,7 @@ public class objectProgramming {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 객체 지향 프로그래밍이란? (Objected Program)
-		
-		
+		// Object Programming
 	}
 
 }

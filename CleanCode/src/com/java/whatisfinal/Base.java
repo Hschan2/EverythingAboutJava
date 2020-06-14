7bce70ba97765cbefad690c3ca5be2b23e262333
HONGSEONGCHAN <seongchan_@naver.com>
2020-06-14 22:35:42 +0900

    WhatIsTheFinal

---
+++ CleanCode/src/com/java/whatisfinal/Base.java
@@ -0,0 +1,11 @@
+package com.java.whatisfinal;
+
+public class Base {
+	public void m1() {
+		
+	}
+	
+	public final void m2() {
+		
+	}
+}
---
+++ CleanCode/src/com/java/whatisfinal/MyFinalClass.java
@@ -0,0 +1,5 @@
+package com.java.whatisfinal;
+
+public final class MyFinalClass { // final 클래스
+	
+}

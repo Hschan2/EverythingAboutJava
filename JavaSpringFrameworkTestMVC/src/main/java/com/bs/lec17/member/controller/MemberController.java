55d834c485ae6b7b55c29c976cd6483d3ea4c8f8
HONGSEONGCHAN <seongchan_@naver.com>
2020-08-14 22:26:01 +0900

    MemeberController_Refactoring

--- JavaSpringFrameworkTestMVC/src/main/java/com/bs/lec17/member/controller/MemberController.java
+++ JavaSpringFrameworkTestMVC/src/main/java/com/bs/lec17/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,37 +24,41 @@ public class MemberController {
 	MemberService service;
 	
 	@RequestMapping(value="/memJoin", method=RequestMethod.POST)
-	public String memJoin(Model model, HttpServletRequest request) {
+	public String memJoin(@ModelAttribute("mem") Member member) {
+//		public String memJoin(Model model, HttpServletRequest request) 기존 방법
 //		HttpServletRequest request -> 사용자 요청에 대한 파라미터 받는 방법
 //		최근 자주 사용하는 방법(커맨드 객체) => public String memJoin(Member member) 로 선언하면 아래 getParameter도 필요 없다 -> getter, setter을 사용하는 방법. html의 name과 같을 때 프로퍼티 사용해 가져옴
 //		service.memberRegister(member.getMemId()...) 로 가져오면 된다
 //		model.addAttribute() 도 필요 없어진다
 		
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
-		String memMail = request.getParameter("memMail");
-		String memPhone1 = request.getParameter("memPhone1");
-		String memPhone2 = request.getParameter("memPhone2");
-		String memPhone3 = request.getParameter("memPhone3");
+//		public String memJoin(@ModelAttribute("mem") Member member)로 선언을 하면 memjoinOk.JSP에서 ${mem.memId}... 로 사용 가능
 		
-		service.memberRegister(memId, memPw, memMail, memPhone1, memPhone2, memPhone3);
+//		String memId = request.getParameter("memId");
+//		String memPw = request.getParameter("memPw");
+//		String memMail = request.getParameter("memMail");
+//		String memPhone1 = request.getParameter("memPhone1");
+//		String memPhone2 = request.getParameter("memPhone2");
+//		String memPhone3 = request.getParameter("memPhone3");
 		
-		model.addAttribute("memId", memId);
-		model.addAttribute("memPw", memPw);
-		model.addAttribute("memMail", memMail);
-		model.addAttribute("memPhone", memPhone1 + " - " + memPhone2 + " - " + memPhone3);
+		service.memberRegister(member.getMemId(), member.getMemPw(), member.getMemMail(), member.getMemPhone1(), member.getMemPhone2(), member.getMemPhone3());
+		
+//		model.addAttribute("memId", memId);
+//		model.addAttribute("memPw", memPw);
+//		model.addAttribute("memMail", memMail);
+//		model.addAttribute("memPhone", memPhone1 + " - " + memPhone2 + " - " + memPhone3);
 		
 		return "memJoinOk";
 	}
 	
 	@RequestMapping(value="/memLogin", method=RequestMethod.POST)
-	public String memLogin(Model model, HttpServletRequest request) {
+	public String memLogin(Model model, @RequestParam("memId") String memId, @RequestParam("memPw") String memPw) {
+//		Model은 데이터를 view에 전달하는 역할
 //		HttpServletRequest request 대신 @RequestParam("memId") String memId(받을 데이터 타입 선언) 로 대체 가능
 //		위 방법으로 할 때 String memId = request.getParameter("memId"); 불필요
 //		@RequestParam(value="memId", Required=true, defaultValue = "1234") required -> 필수냐 아니냐, defaultValue -> 넘어온 값이 없을 시 
 		
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
+//		String memId = request.getParameter("memId");
+//		String memPw = request.getParameter("memPw");
 		
 		Member member = service.memberSearch(memId, memPw);
 		
--- JavaSpringFrameworkTestMVC/src/main/webapp/WEB-INF/views/memJoinOk.jsp
+++ JavaSpringFrameworkTestMVC/src/main/webapp/WEB-INF/views/memJoinOk.jsp
@@ -7,12 +7,13 @@
 </head>
 <body>
 	<h1> 회원 가입 성공 </h1>
-	아이디 : ${memId}<br />
-	비밀번호 : ${memPw}<br />
-	이메일 : ${memMail} <br />
-	핸드폰 : ${memPhone} <br />
+	아이디 : ${mem.memId}<br />
+	비밀번호 : ${mem.memPw}<br />
+	이메일 : ${mem.memMail} <br />
+	핸드폰 : ${mem.memPhone1} - ${mem.memPhone2} - ${mem.memPhone3} <br />
 	<!-- public String memJoin(Member member)로 선언했을 시. ${member.memId} ... 로 가져오면 된다 -->
 	<!-- 핸드폰은 ${member.memPhone1}-${member.memPhone1}-${member.memPhone1}로 가져오면 된다 -->
+	<!-- Controller에서 @ModelAttribute("mem") Member member로 선언했을 경우 -->
 	
 	<a href="/lec17/resources/html/login.html">로그인</a>
 </body>

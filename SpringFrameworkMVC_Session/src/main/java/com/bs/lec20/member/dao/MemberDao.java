8b5b84fdd35856ecc80e41c765ad87ccdc8780a7
HONGSEONGCHAN <seongchan_@naver.com>
2020-09-08 12:40:58 +0900

    JDBC without Template

--- SpringFrameworkMVC_Session/src/main/java/com/bs/lec20/member/dao/MemberDao.java
+++ SpringFrameworkMVC_Session/src/main/java/com/bs/lec20/member/dao/MemberDao.java
@@ -1,5 +1,11 @@
 package com.bs.lec20.member.dao;
 
+import java.io.Console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,42 +15,174 @@ import com.bs.lec20.member.Member;
 
 @Repository
 public class MemberDao implements IMemberDao {
-
+	
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	private String userid = "hseongchan2";
+	private String userpw = "448744";
+	
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
 	private HashMap<String, Member> dbMap;
 	
 	public MemberDao() {
-		dbMap = new HashMap<String, Member>();
+//		dbMap = new HashMap<String, Member>();
 	}
 	
+	// 가입
 	@Override
-	public Map<String, Member> memberInsert(Member member) {
+	public int memberInsert(Member member) {
 		
-		dbMap.put(member.getMemId(), member);
-		return dbMap;
+		int result = 0;
+		
+		try { // DB 연결할 때는 무조건 try catch문!
+			Class.forName(driver); // Driver 로딩
+			conn = DriverManager.getConnection(url, userid, userpw); // DB 연결
+			String sql = "INSERT INTO member (memId, memPw, memMail) values (?, ?, ?)"; // DB 데이터 삽입 쿼리
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemId()); // Id 값
+			pstmt.setString(2, member.getMemPw()); // Pw 값
+			pstmt.setString(3, member.getMemMail()); // Mail 값
+			result = pstmt.executeUpdate(); // 삽입 실행
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally { // 자원 해제 (연결 해제)
+			try {
+				if(pstmt != null) pstmt.close(); // pstmt가 실행중이면 닫기
+				if(conn != null) conn.close(); // db 연결중이면 끊기
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+//		dbMap.put(member.getMemId(), member);
+		return result;
 		
 	}
 
+	// 로그인
 	@Override
 	public Member memberSelect(Member member) {
 		
-		Member mem = dbMap.get(member.getMemId());
+		Member mem = null;
+		
+		try {
+			Class.forName(driver); 
+			conn = DriverManager.getConnection(url, userid, userpw);
+			String sql = "SELECT * FROM member WHERE memId = ? and memPw = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemId());
+			pstmt.setString(2, member.getMemPw());
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String memId = rs.getString("memid");
+				String memPw = rs.getString("mempw");
+				String memMail = rs.getString("memMail");
+				int memPurcNum = rs.getInt("memPurcNum");
+				
+				mem = new Member();
+				mem.setMemId(memId);
+				mem.setMemPw(memPw);
+				mem.setMemMail(memMail);
+				mem.setMemPurcNum(memPurcNum);
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+//		Member mem = dbMap.get(member.getMemId());
 		return mem;
 		
 	}
 
+	// 정보 수정
 	@Override
-	public Member memberUpdate(Member member) {
+	public int memberUpdate(Member member) {
 		
-		dbMap.put(member.getMemId(), member);
-		return dbMap.get(member.getMemId());
+		int result = 0;
+		
+		try {
+			Class.forName(driver); 
+			conn = DriverManager.getConnection(url, userid, userpw);
+			String sql = "UPDATE member SET memPw = ? AND memMail = ? WHERE memId = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemPw());
+			pstmt.setString(2, member.getMemMail());
+			pstmt.setString(3, member.getMemId());
+			result = pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+//		dbMap.put(member.getMemId(), member);
+		return result;
 		
 	}
 
 	@Override
-	public Map<String, Member> memberDelete(Member member) {
+	public int memberDelete(Member member) {
 		
-		dbMap.remove(member.getMemId());
-		return dbMap;
+		int result = 0;
+		
+		try {
+			Class.forName(driver); 
+			conn = DriverManager.getConnection(url, userid, userpw);
+			String sql = "DELETE member WHERE memId = ? AND memPw = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemId());
+			pstmt.setString(2, member.getMemPw());
+			result = pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+//		dbMap.remove(member.getMemId());
+		return result;
 		
 	}
 
--- SpringFrameworkMVC_Session/src/main/java/com/bs/lec20/member/service/MemberService.java
+++ SpringFrameworkMVC_Session/src/main/java/com/bs/lec20/member/service/MemberService.java
@@ -18,13 +18,20 @@ public class MemberService implements IMemberService {
 	
 	@Override
 	public void memberRegister(Member member) {
-		printMembers(dao.memberInsert(member));
+//		printMembers(dao.memberInsert(member));
+		int result = dao.memberInsert(member);
+		
+		if(result == 0) System.out.println("가입 실패");
+		else System.out.println("가입 성공");
 	}
 
 	@Override
 	public Member memberSearch(Member member) {
+		
 		Member mem = dao.memberSelect(member);
-		printMember(mem);
+		
+		if(mem == null) System.out.println("로그인 실패");
+		else System.out.println("로그인 성공");
 		
 		return mem;
 	}
@@ -32,36 +39,48 @@ public class MemberService implements IMemberService {
 	@Override
 	public Member memberModify(Member member) {
 		
-		Member memAft = dao.memberUpdate(member);
-		printMember(memAft);
+		int result = dao.memberUpdate(member);
 		
-		return memAft;
+		if(result == 0) {
+			System.out.println("정보 수정 실패");
+			return null; // 실패했으니 null을 반환
+		} else {
+			System.out.println("정보 수정 성공");
+		}
+		
+		return member;
 	}
 	
 	@Override
-	public void memberRemove(Member member) {
-		printMembers(dao.memberDelete(member));
+	public int memberRemove(Member member) {
+		
+		int result = dao.memberDelete(member);
+		
+		if(result == 0) System.out.println("회원 탈퇴 실패"); 
+		else System.out.println("회원 탈퇴 성공"); // result == 1
+		
+		return result;
 	}
 	
-	private void printMembers(Map<String, Member> map) {
-		
-		Set<String> keys = map.keySet();
-		Iterator<String> iterator = keys.iterator();
-		
-		while (iterator.hasNext()) {
-			String key = iterator.next();
-			Member mem = map.get(key);
-			printMember(mem);
-		}
-		
-	}
-	
-	private void printMember(Member mem) {
-		
-		System.out.print("ID:" + mem.getMemId() + "\t");
-		System.out.print("|PW:" + mem.getMemPw() + "\t");
-		System.out.print("|MAIL:" + mem.getMemMail() + "\n");
-		
-	}
-
+	// 데이터베이스 쓰기 전 (로컬)
+//	private void printMembers(Map<String, Member> map) {
+//		
+//		Set<String> keys = map.keySet();
+//		Iterator<String> iterator = keys.iterator();
+//		
+//		while (iterator.hasNext()) {
+//			String key = iterator.next();
+//			Member mem = map.get(key);
+//			printMember(mem);
+//		}
+//		
+//	}
+//	
+//	private void printMember(Member mem) {
+//		
+//		System.out.print("ID:" + mem.getMemId() + "\t");
+//		System.out.print("|PW:" + mem.getMemPw() + "\t");
+//		System.out.print("|MAIL:" + mem.getMemMail() + "\n");
+//		
+//	}
 }

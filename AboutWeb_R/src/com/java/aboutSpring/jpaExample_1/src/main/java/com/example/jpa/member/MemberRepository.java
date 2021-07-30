f805c3e440c3f97275fd38f47c07ba1dcb46fa6b
HONGSEONGCHAN <seongchan_@naver.com>
2021-07-30 16:18:07 +0900

    Modify: aboutSpring 프로젝트 폴더 변경

--- AboutWeb_R/src/com/java/aboutSpring/jpaExample_1/src/main/java/com/example/jpa/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpa.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-
-}

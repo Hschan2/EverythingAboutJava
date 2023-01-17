92648e0574467bb250dcad68977ba15b9d51ba9b
Hchan2 <seongchan_@naver.com>
2023-01-17 18:38:23 +0900

    사진 에디터 프로젝트를 위한 학습 - 3

--- Just-Make-Something/pictureFilter/editor-project/src/component/Main.js
+++ Just-Make-Something/pictureFilter/editor-project/src/component/Main.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/main.scss'
-import DEFAULT_OPTIONS from '../constant/options'
 import { GrRotateLeft, GrRotateRight } from 'react-icons/gr'
 import { CgMergeVertical, CgMergeHorizontal } from 'react-icons/cg'
 import { IoMdUndo, IoMdRedo, IoIosImage } from 'react-icons/io'
 
 function Main() {
+    const filterElement = [
+        {
+            name: 'brightness',
+            maxValue: 200,
+        },
+        {
+            name: 'contrast',
+            maxValue: 200,
+        },
+        {
+            name: 'saturate',
+            maxValue: 200,
+        },
+        {
+            name: 'grayscale',
+            maxValue: 100,
+        },
+        {
+            name: 'sepia',
+            maxValue: 100,
+        },
+        {
+            name: 'hueRotate',
+            maxValue: 360,
+        },
+        {
+            name: 'blur',
+            maxValue: 20,
+        }
+    ]
+    const [property, setProperty] = useState(
+        {
+            name: 'brightness',
+            maxValue: 200,
+        }
+    )
+    const [state, setState] = useState({
+        image: '',
+        brightness: 100,
+        grayscale: 0,
+        sepia: 0,
+        saturate: 100,
+        contrast: 100,
+        hueRotate: 0,
+        blur: 0,
+        rotate: 0,
+        vertical: 1,
+        horizontal: 1
+    })
+    const inputHandle = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+    const imageHandle = (e) => {
+        if (e.target.files.length !== 0) {
+            const reader = new FileReader()
+            reader.onload = () => {
+                setState({
+                    ...state,
+                    image: reader.result
+                })
+            }
+            reader.readAsDataURL(e.target.files[0])
+        }
+    }
+    const leftRotate = (e) => {
+        setState({
+            ...state,
+            rotate: state.rotate - 90
+        })
+    }
+    const rightRotate = (e) => {
+        setState({
+            ...state,
+            rotate: state.rotate + 90
+        })
+    }
+
     return (
         <div className='image_editor'>
             <div className='card'>
@@ -18,8 +97,8 @@ function Main() {
                             <div className='filter_section'>
                                 <span></span>
                                 <div className='filter_key'>
-                                    {DEFAULT_OPTIONS.map((v, i) => 
-                                        <button key={i}>{v.name}</button>
+                                    {filterElement.map((v, i) => 
+                                        <button className={property.name === v.name ? 'active' : ''} onClick={() => setProperty(v)} key={i}>{v.name}</button>
                                     )}
                                 </div>
                             </div>
@@ -28,13 +107,13 @@ function Main() {
                                     <label htmlFor='range'>Rotate</label>
                                     <span>100%</span>
                                 </div>
-                                <input type='range' />
+                                <input name={property.name} onChange={inputHandle} value={state[property.name]} max={property.maxValue} type='range' />
                             </div>
                             <div className='rotate'>
                                 <label htmlFor=''>Rotate & Flip</label>
                                 <div className='icon'>
-                                    <div><GrRotateLeft /></div>
-                                    <div><GrRotateRight /></div>
+                                    <div onClick={leftRotate}><GrRotateLeft /></div>
+                                    <div onClick={rightRotate}><GrRotateRight /></div>
                                     <div><CgMergeVertical /></div>
                                     <div><CgMergeHorizontal /></div>
                                 </div>
@@ -47,17 +126,23 @@ function Main() {
                     </div>
                     <div className='image_section'>
                         <div className='image'>
-                            <label htmlFor='choose'>
-                                <IoIosImage />
-                                <span>Choose Image</span>
-                            </label>
+                            {
+                                state.image ? <img style={{
+                                    filter: `brightness(${state.brightness}%) grayscale(${state.grayscale}%) sepia(${state.sepia}%) saturate(${state.saturate}%) contrast(${state.contrast}%) hue-rotate(${state.hueRotate}deg) blur(${state.blur}px)`,
+                                    transform: `rotate(${state.rotate}deg) scale(${state.vertical}, ${state.horizontal})`
+                                }} src={state.image} alt='' /> :
+                                    <label htmlFor='choose'>
+                                        <IoIosImage />
+                                        <span>Choose Image</span>
+                                    </label>
+                            }
                         </div>
                         <div className='image_select'>
                             <button className='undo'><IoMdUndo /></button>
                             <button className='redo'><IoMdRedo /></button>
                             <button className='crop'>Crop Image</button>
                             <label htmlFor='choose'>Choose Image</label>
-                            <input type='file' />
+                            <input type='file' id='choose' onChange={imageHandle} />
                         </div>
                     </div>
                 </div>

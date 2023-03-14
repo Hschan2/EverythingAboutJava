ef45fc14119716bebd46f6eae0ad9da4c554ebea
Hchan2 <seongchan_@naver.com>
2023-03-14 18:51:56 +0900

    마크다운 목록 정리, 가사 클릭 시 목소리 출력

--- Just-Make-Something/learnfrompop/src/App.js
+++ Just-Make-Something/learnfrompop/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { lyrics } from './constants/pop'
 
 function App() {
+  const getLyrics = lyrics.split('\n').map(v => v.trim()).filter(v => v.length > 0);
+
+  const speakText = (e) => {
+    e.preventDefault();
+    const text = e.target.textContent;
+    const voices = window.speechSynthesis.getVoices();
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = voices[127];
+    speechSynthesis.speak(utterance);
+  }
+
   return (
     <div className="App">
-      
+      {getLyrics?.map((lyrics, index) => (
+        <p key={index} onClick={speakText}>{lyrics}</p>
+      ))}
     </div>
   );
 }

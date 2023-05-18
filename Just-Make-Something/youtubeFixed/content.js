38ce9dde37070e53ebfbb471d2feff01f6a59ca1
Hchan2 <seongchan_@naver.com>
2023-05-18 22:35:55 +0900

    구글 익스텐션, 유튜브 영상 위치 고정 1차 구현

--- Just-Make-Something/youtubeFixed/content.js
+++ Just-Make-Something/youtubeFixed/content.js
@@ -1,21 +1,35 @@
-function fixYouTubeVideoPosition() {
-    const videoElement = document.querySelector('video'); // 유튜브 영상 요소 선택
+import "./content.css";
 
+// YouTube 영상을 고정시키는 함수
+function fixVideoPosition() {
+    const videoElement = document.querySelector("#player-theater-container");
     if (videoElement) {
-        const originalPosition = videoElement.getBoundingClientRect(); // 영상의 현재 위치 정보 저장
-
-        // 스크롤 이벤트 핸들러
-        const handleScroll = () => {
-            const scrollY = window.scrollY; // 현재 스크롤 위치
-
-            // 영상의 위치를 고정하기 위해 스크롤 위치에 따라 영상의 스타일을 조정
-            videoElement.style.position = 'fixed';
-            videoElement.style.top = originalPosition.top - scrollY + 'px';
-        };
-
-        window.addEventListener('scroll', handleScroll); // 스크롤 이벤트 리스너 등록
+      videoElement.classList.add("fixed-video");
     }
 }
 
-// 페이지 로드 완료 시 영상 고정 함수 호출
-window.addEventListener('load', fixYouTubeVideoPosition);
+// YouTube 영상 고정 클래스를 제거하는 함수
+function unfixVideoPosition() {
+    const videoElement = document.querySelector("#player-theater-container");
+    if (videoElement) {
+      videoElement.classList.remove("fixed-video");
+    }
+}
+
+// 스크롤 이벤트 핸들러
+function handleScroll() {
+    const scrollY = window.scrollY;
+    const threshold = 100; // 맨 위로 올릴 때 고정 클래스를 제거할 스크롤 임계값
+
+    if (scrollY > threshold) {
+      fixVideoPosition();
+    } else {
+      unfixVideoPosition();
+    }
+}
+
+// 스크롤 이벤트 리스너 등록
+window.addEventListener("scroll", handleScroll);
+
+// 페이지 로드 시 YouTube 영상 고정
+window.addEventListener("load", fixVideoPosition);

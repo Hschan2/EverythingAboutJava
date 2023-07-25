8a4430872a2cde7e7f8a2fc9c4c31471200f7e80
Hchan2 <seongchan_@naver.com>
2023-07-25 17:51:16 +0900

    번호로 목소리 지정이 아닌, 이름으로 목소리 지정

--- Just-Make-Something/learnfrompop/src/constants/SpeakVoice.js
+++ Just-Make-Something/learnfrompop/src/constants/SpeakVoice.js
@@ -2,7 +2,21 @@ export const speakText = (e) => {
     e.preventDefault();
     const text = e.target.textContent;
     const voices = window.speechSynthesis.getVoices();
+    let voice = null;
+
+    for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === 'Microsoft Zira Desktop - English (United States)' || voices[i].lang === 'en-US') {
+            voice = voices[i];
+            break;
+        }
+    }
+
+    if (!voice) {
+        console.error('Voice not found for speech synthesis.');
+        return;
+    }
+
     const utterance = new SpeechSynthesisUtterance(text);
-    utterance.voice = voices[127];
+    utterance.voice = voice;
     speechSynthesis.speak(utterance);
 }

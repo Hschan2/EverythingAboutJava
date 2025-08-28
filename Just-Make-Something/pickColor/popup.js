b547a3bf8cce82ab0669643d9de2d1faf2f5dbb4
Hchan2 <seongchan_@naver.com>
2025-08-28 16:28:46 +0900

    update: 복사 버튼 추가

--- Just-Make-Something/pickColor/popup.js
+++ Just-Make-Something/pickColor/popup.js
@@ -5,6 +5,7 @@ window.onload = function () {
   const modalContent = document.querySelector(".modal-content");
   const closeBtn = document.querySelector(".close");
   const colorValue = document.getElementById("colorValue");
+  const copyButton = document.getElementById("copyButton");
 
   // 색상 선택하기
   const pickColor = async () => {
@@ -16,13 +17,34 @@ window.onload = function () {
     modal.style.display = "block";
   }
 
+  // 색상 값 복사하기
+  const copyColor = async () => {
+    try {
+      await navigator.clipboard.writeText(colorValue.textContent);
+
+      copyButton.textContent = "복사 성공";
+
+      setTimeout(() => {
+        copyButton.textContent = "복사";
+      }, 3000);
+
+    } catch (err) {
+      console.error("클립보드 복사 실패:", err);
+      alert("클립보드 복사에 실패했습니다.");
+    }
+  };
+
   button.addEventListener("click", pickColor);
+
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
+
   window.addEventListener("click", (event) => {
     if (event.target === modal) {
       modal.style.display = "none";
     }
   });
+
+  copyButton.addEventListener("click", copyColor);
 }
